4d9c38f82697bb9d965d2c8e0e211e7ea07eabd4
albin450 <aarooralvishnu@gmail.com>
2025-07-27 12:42:51 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,13 @@
-import type { NextConfig } from 'next'
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  output: "export",
   images: {
-    domains: ['your-image-domain.com'],
+    unoptimized: true,
   },
-}
+  typescript: {
+    // ignoreBuildErrors: true,
+  },
+};
 
-export default nextConfig
+export default nextConfig;
